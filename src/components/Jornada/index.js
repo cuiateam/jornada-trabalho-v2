74071e23850e404747894829bdf23e0788c153cf
artur <arthurjhonys@gmail.com>
2020-05-25 15:05:27 -0400

    Adição do evento para calcular Horas

--- src/components/Jornada/index.js
+++ src/components/Jornada/index.js
@@ -14,8 +14,66 @@ export default function Jornada() {
     const [entrada2, setEntrada2] = useState()
     const [saida2, setSaida2] = useState()
 
-    function handleClickCalcular(){
-  
+    const recordFromTotalMinutes = (total) => {
+
+        let h = Math.floor(total / 60)
+        let m = total - h * 60
+        let desc = String(h).concat(":").concat(("00" + String(m)).slice(-2))
+
+        console.log(`h: ${h}`)
+        console.log(`m: ${m}`)
+        console.log(`desc: ${desc}`)
+
+        return new record(h, m, total, desc)
+    }
+
+    function record(h, m, t, d) {
+
+        this.hours = h
+        this.minutes = m
+        this.total = t
+        this.desc = d
+
+    }
+
+    function newRecord(valor) {
+
+        if (valor === "")
+            return null
+        var h = parseInt(valor.split(":")[0])
+        var m = parseInt(valor.split(":")[1])
+        var t = h * 60 + m
+        return new record(h, m, t, valor)
+    }
+
+
+    function handleClickCalcular() {
+
+        let minutosTrabalhado = 0
+        let minutosATrabalhar = 0
+        let totalCalculo = 0
+        let horaSaida = 0
+        let valorJornada = newRecord(jornada)
+        let entradaFinal = newRecord(entrada2)
+        // Define o parâmetro de Jornada de Trabalho
+        let parametroJornadaTrabalho = jornada === null ? recordFromTotalMinutes(525) : valorJornada
+        console.log(`parametroJornadaTrabalho: ${parametroJornadaTrabalho}`)
+        //TODO: lista criada para receber valor de todos os campos que forem criado em momento de execução.
+        let entrada = []
+        let saida = []
+
+        entrada = newRecord(entrada1)
+        saida = newRecord(saida1)
+
+        minutosTrabalhado = saida.total - entrada.total
+
+        //formula
+        minutosATrabalhar = parametroJornadaTrabalho.total - minutosTrabalhado
+        totalCalculo = entradaFinal.total + minutosATrabalhar
+        horaSaida = recordFromTotalMinutes(totalCalculo)
+
+        setSaida2(horaSaida.desc)
+
     }
 
     function handleClickLimpar() {
@@ -24,6 +82,7 @@ export default function Jornada() {
         setEntrada2("")
         setSaida2("")
     }
+
 
     return (
         <div className="jornada">
@@ -49,26 +108,26 @@ export default function Jornada() {
                             <Col className="text-center">
                                 Saída
                             </Col>
-                            
+
                         </Form.Row>
-                            
+
                         <Form.Row>
                             <Col>
-                                <Form.Control className="input" type="time" id="entrada1" value={entrada1} onChange={(e) => setEntrada1(e.target.value)}/>
+                                <Form.Control className="input" type="time" id="entrada1" value={entrada1} onChange={(e) => setEntrada1(e.target.value)} />
                             </Col>
                             <Col>
-                                <Form.Control className="input" type="time" id="saida1" value={saida1} onChange={(e) => setSaida1(e.target.value)}/>
+                                <Form.Control className="input" type="time" id="saida1" value={saida1} onChange={(e) => setSaida1(e.target.value)} />
                             </Col>
                         </Form.Row>
                         <Form.Row>
                             <Col>
-                                <Form.Control className="input" type="time" id="entradaFinal" value={entrada2} onChange={(e)=> setEntrada2(e.target.value)} />
+                                <Form.Control className="input" type="time" id="entradaFinal" value={entrada2} onChange={(e) => setEntrada2(e.target.value)} />
                             </Col>
                             <Col>
-                                <Form.Control className="input" type="time" id="saidaFinal" readOnly value={saida2} onChange={(e)=> setSaida2(e.target.value)} />
+                                <Form.Control className="input" type="time" id="saidaFinal" readOnly value={saida2} onChange={(e) => setSaida2(e.target.value)} />
                             </Col>
                         </Form.Row>
-                           
+
                     </Form>
 
                     <div className="button-group">
