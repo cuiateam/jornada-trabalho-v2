ab03d97f3ff3945710d6964db95a51c11af71d52
artur <arthurjhonys@gmail.com>
2020-05-29 23:50:21 -0400

    evento adição de campo
    criação do campo de adição do campos e iniciado o carregamento do array com os valores dos campos

--- src/components/Jornada/index.js
+++ src/components/Jornada/index.js
@@ -6,23 +6,25 @@ import Button from 'react-bootstrap/Button'
 
 import './styles.css'
 
+const campoPadrao = [
+    {
+        horasEntrada: "",
+        horasSaida: ""
+    }
+]
+
 export default function Jornada() {
 
     const [jornada, setJornada] = useState("08:45")
-    const [entrada1, setEntrada1] = useState()
-    const [saida1, setSaida1] = useState()
     const [entrada2, setEntrada2] = useState()
     const [saida2, setSaida2] = useState()
+    const [camposEntradaSaida, setCamposEntradaSaida] = useState(campoPadrao)
 
     const recordFromTotalMinutes = (total) => {
 
         let h = Math.floor(total / 60)
         let m = total - h * 60
         let desc = String(h).concat(":").concat(("00" + String(m)).slice(-2))
-
-        console.log(`h: ${h}`)
-        console.log(`m: ${m}`)
-        console.log(`desc: ${desc}`)
 
         return new record(h, m, total, desc)
     }
@@ -38,32 +40,44 @@ export default function Jornada() {
 
     function newRecord(valor) {
 
-        if (valor === "")
-            return null
         var h = parseInt(valor.split(":")[0])
         var m = parseInt(valor.split(":")[1])
         var t = h * 60 + m
+
         return new record(h, m, t, valor)
     }
 
-
     function handleClickCalcular() {
-
         let minutosTrabalhado = 0
         let minutosATrabalhar = 0
         let totalCalculo = 0
         let horaSaida = 0
+        let entrada
+        let saida
+        let totalMinutosEntrada
         let valorJornada = newRecord(jornada)
         let entradaFinal = newRecord(entrada2)
+   
         // Define o parâmetro de Jornada de Trabalho
-        let parametroJornadaTrabalho = jornada === null ? recordFromTotalMinutes(525) : valorJornada
-        console.log(`parametroJornadaTrabalho: ${parametroJornadaTrabalho}`)
-        //TODO: lista criada para receber valor de todos os campos que forem criado em momento de execução.
-        let entrada = []
-        let saida = []
+        let parametroJornadaTrabalho = jornada == null ? recordFromTotalMinutes(525) : valorJornada
 
-        entrada = newRecord(entrada1)
-        saida = newRecord(saida1)
+        //TODO: lista criada para receber valor de todos os campos que forem criado em momento de execução.
+
+
+        entrada = camposEntradaSaida.map((item) => (newRecord(item.horasEntrada)))
+        
+
+        saida = camposEntradaSaida.map((item) => (newRecord(item.horasSaida)))
+        
+        console.log(entrada)
+        console.log(entrada.length)
+
+        for(let i=0; i <= entrada.length; i++){
+             totalMinutosEntrada += entrada[i].record.total 
+        }
+       
+        console.log(totalMinutosEntrada)
+
 
         minutosTrabalhado = saida.total - entrada.total
 
@@ -77,12 +91,26 @@ export default function Jornada() {
     }
 
     function handleClickLimpar() {
-        setEntrada1("")
-        setSaida1("")
         setEntrada2("")
         setSaida2("")
+        setCamposEntradaSaida(campoPadrao)
     }
 
+    const handleFieldChange = (e) => {
+        let tempFilds = [...camposEntradaSaida]
+
+        tempFilds[e.target.dataset.id][e.target.name] = e.target.value
+
+        setCamposEntradaSaida(tempFilds)
+    }
+
+    function handleClickAdicionar() {
+        if (entrada2) {
+            setEntrada2("")
+        }
+
+        setCamposEntradaSaida(campos => [...campos, { horasEntrada: "", horasSaida: "" }])
+    }
 
     return (
         <div className="jornada">
@@ -111,28 +139,58 @@ export default function Jornada() {
 
                         </Form.Row>
 
-                        <Form.Row>
-                            <Col>
-                                <Form.Control className="input" type="time" id="entrada1" value={entrada1} onChange={(e) => setEntrada1(e.target.value)} />
-                            </Col>
-                            <Col>
-                                <Form.Control className="input" type="time" id="saida1" value={saida1} onChange={(e) => setSaida1(e.target.value)} />
-                            </Col>
-                        </Form.Row>
-                        <Form.Row>
-                            <Col>
-                                <Form.Control className="input" type="time" id="entradaFinal" value={entrada2} onChange={(e) => setEntrada2(e.target.value)} />
-                            </Col>
-                            <Col>
-                                <Form.Control className="input" type="time" id="saidaFinal" readOnly value={saida2} onChange={(e) => setSaida2(e.target.value)} />
-                            </Col>
-                        </Form.Row>
+                        {camposEntradaSaida.map((item, index) => (
 
+                            <Form.Row key={index}>
+                                <Col>
+                                    <Form.Control
+                                        name="horasEntrada"
+                                        className="input"
+                                        type="time"
+                                        data-id={index}
+                                        value={item.horasEntrada}
+                                        onChange={handleFieldChange} />
+                                </Col>
+                                <Col>
+                                    <Form.Control
+                                        name="horasSaida"
+                                        className="input"
+                                        type="time"
+                                        data-id={index}
+                                        value={item.horasSaida}
+                                        onChange={handleFieldChange} />
+                                </Col>
+                            </Form.Row>
+                        ))}
+
+                        <Form.Row>
+                            <Col>
+                                <Form.Control
+                                    className="input"
+                                    type="time"
+                                    id="entradaFinal"
+                                    value={entrada2}
+                                    onChange={(e) => setEntrada2(e.target.value)} />
+                            </Col>
+                            <Col>
+                                <Form.Control
+                                  
+                                    className="input"
+                                    type="time"
+                                    id="saidaFinal"
+                                    readOnly
+                                    value={saida2}
+                                    onChange={(e) => setSaida2(e.target.value)} />
+                            </Col>
+                        </Form.Row>
                     </Form>
 
                     <div className="button-group">
                         <Button className="button" variant="primary" onClick={() => handleClickCalcular()}>
                             Calcular
+                        </Button>
+                        <Button className="button" variant="secondary" onClick={() => handleClickAdicionar()}>
+                            Adicionar
                         </Button>
                         <Button className="button" variant="danger" onClick={() => handleClickLimpar()}>
                             Limpar
